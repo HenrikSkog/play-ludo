e614c7dc8be5209dd821372aad176fd256727d9c
Erlend0703 <Sorknerl@gmail.com>
2021-04-19 13:48:02 +0200

    PlayerTest er ferdig

---
+++ src/test/java/org/ludo/PlayerTest.java
@@ -0,0 +1,83 @@
+package org.ludo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.ludo.gameLogic.Areas;
+import org.ludo.gameLogic.Piece;
+import org.ludo.gameLogic.Player;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PlayerTest {
+    private ArrayList<Player> players = new ArrayList<>();
+    private ArrayList<Piece> pieces = new ArrayList<>();
+
+    @BeforeEach
+    public void init() {
+        for (int i = 0; i < 4; i++) {
+            Player player = new Player("player", i);
+            player.initializePieces();
+            players.add(player);
+        }
+        for (int i = 0; i < 4; i++) {
+            Piece piece = new Piece(i, 0);
+            pieces.add(piece);
+        }
+    }
+
+
+        @Test
+        @DisplayName("Testing creating a player")
+        public void testPlayer () {
+            Player player = new Player("Henrik", 2);
+            assertEquals("Henrik", player.getName());
+            assertEquals(player.getColorIndex(), 2);
+        }
+
+        @Test
+        @DisplayName("Testing if name is length = 0")
+        public void testSetName () {
+            assertThrows(IllegalArgumentException.class, () -> {
+                Player player = new Player("", 2);
+            });
+        }
+
+        @Test
+        @DisplayName("Test initialize pieces")
+        public void testInitPieces (){
+        for(Player player : players){
+            assertEquals(4, player.getPieces().size());
+            for(Piece piece : player.getPieces()){
+                assertEquals(player.getColorIndex(), piece.getColorIndex());
+                assertEquals(Areas.YARD, piece.getBoardArea());
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Test get pieces in Yard")
+    public void testgetPiecesInYard(){
+        Player player1 = players.get(0);
+        assertEquals(4, player1.getPiecesInYard().size());
+    }
+
+    @Test
+    @DisplayName("Test Pieces")
+    public void testSetPieces(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            Player player = players.get(0);
+            player.setPieces(pieces);
+        });
+    }
+
+    @Test
+    @DisplayName("Test for all pieces in yard")
+    public void testPiecesinYard(){
+        Player player1 = players.get(0);
+        assertEquals(true, player1.hasAllPiecesInYard());
+    }
+}
